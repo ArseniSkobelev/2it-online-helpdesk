421e63616dde696819fbd6c714bf09b8083243e1
ARSENI SKOBELEV <arseni.skobelev0@gmail.com>
2021-11-01 13:00:57 +0100

    Completed frontend

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -21,12 +21,13 @@
 
 
 function send() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log("nice")
-        }
-    };
-    xhttp.open("POST", "http://06d4-195-1-199-142.eu.ngrok.io/form?firstname=john&lastname=doe&email=oliver@oliver.com&title=Something&message=So+i+am+a+gay+man+yes+goood", true);
-    xhttp.send();
+    var data = "firstname=" + document.getElementById("input1").value + "&lastname=" + document.getElementById("input2").value + "&email=" + document.getElementById("input3").value + "&title=" + document.getElementById("input4").value + "&message=" + document.getElementById("input5").value
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log("nice")
+      }
+  };
+  xhttp.open("POST", "http://3.16.42.132:3000/form?" + data, true);
+  xhttp.send();
 }
