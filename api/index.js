f75e26c83f7a53ac124e3a90e550bb029d7117a0
NUB31 <oliverlhs@outlook.com>
2021-11-01 14:26:30 +0100

    frontend fix for hostinger/ backend fix send

--- api/index.js
+++ api/index.js
@@ -69,6 +69,7 @@ const send_email = async () => {
             connection.release(); 
                 mailFrom.subject = "Ticket submited with id #" + rows[0].id;
                 mailTo.subject = "Ticket submited with id #" + rows[0].id;
+                mailTo.text = rows[0].message + "\n Check dashboard for more info";
                 transporter.sendMail(mailFrom, function(error, info){
                     if (error) {
                         console.log(error);
