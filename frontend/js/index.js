73967a6ff1f35123b89f1ace79f2d4c97f61c465
NUB31 <oliverlhs@outlook.com>
2021-11-01 14:26:30 +0100

    frontend fix for hostinger/ backend fix send

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,33 +1,15 @@
-// var xmlhttp = ajaxReq();
-// var url = "http://06d4-195-1-199-142.eu.ngrok.io/form?firstname=john&lastname=doe&email=oliver@oliver.com&title=Something&message=So+i+am+a+gay+man+yes+goood";
-// var params = "your post body parameters";
-// xmlhttp.open("POST", url, true); // set true for async, false for sync request
-// xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-// xmlhttp.send(params); // or null, if no parameters are passed
-
-// xmlhttp.onreadystatechange = function () {
-//     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-//        try {
-//            var obj = JSON.parse(xmlhttp.responseText);
-
-//            // do your work here
-
-//        } catch (error) {
-//            throw Error;
-//        }
-//     }
-// }
-
-
-
 function send() {
     var data = "firstname=" + document.getElementById("input1").value + "&lastname=" + document.getElementById("input2").value + "&email=" + document.getElementById("input3").value + "&title=" + document.getElementById("input4").value + "&message=" + document.getElementById("input5").value
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log("nice")
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) return;
+      if (xhr.status >= 200 && xhr.status < 300) {
+        window.location.replace("../success.html")
+      } else {
+        window.location.replace("../error.html")
       }
-  };
-  xhttp.open("POST", "http://3.16.42.132:3000/form?" + data, true);
-  xhttp.send();
+      console.log('Request sent');
+    };
+    xhr.open('POST', 'http://3.16.42.132:3000/form?' + data);
+    xhr.send();
 }
