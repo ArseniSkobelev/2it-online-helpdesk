f2287c9c9c2e352e108c8113f123ceb6ebdd74f4
Arseni Skobelev <arseni.skobelev0@gmail.com>
2021-11-02 21:01:30 +0100

    working on dashboard; login page frontend done

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,14 +1,38 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
 
 function createWindow () {
     const win = new BrowserWindow({
         width: 1920,
-        height: 1080
+        height: 1080,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
     })
 
-    win.loadFile('./src/index.html')
+    win.loadFile(`./src/login.html`)
+
+    ipcMain.on("unauthenticated", (event) => {
+        win.loadFile(`./src/login.html`)
+    })
+
+    ipcMain.on("authenticated", async event => {
+        win.loadFile('./src/index.html');
+
+    })
+
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
 })
