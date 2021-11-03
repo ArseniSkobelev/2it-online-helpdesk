02d0705bbe745658d315dc037fba42b3ba0f9237
Oliver Stene <oste@2ISF.alesund.vgs.no>
2021-11-03 09:01:54 +0100

    fixed backend

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,13 +1,13 @@
 function send() {
-  let data = "name="+document.getElementById("name").value+"&email="+document.getElementById("email").value+"&title="+document.getElementById("title").value+"&phonenum="+document.getElementById("phonenum").value+"&avdeling="+document.getElementById("avdeling").value+"&message="+document.getElementById("message").value;
+  let data = "name="+document.getElementById("name").value+"&email="+document.getElementById("email").value+"&title="+document.getElementById("title").value+"&phonenum="+document.getElementById("phonenum").value+"&avdeling="+document.getElementById("avd").value+"&message="+document.getElementById("text").value;
 
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.readyState !== 4) return;
     if (xhr.status >= 200 && xhr.status < 300) {
-      window.location.replace("../success.html")
+      window.location.replace("../helpdesk/success.html")
     } else {
-      window.location.replace("../error.html")
+      window.location.replace("../helpdesk/error.html")
     }
     console.log('Request sent');
   };
