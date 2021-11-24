8e6d007d00d751a6326579ba4b983dfc482e9496
NUB31 <oliverlhs@outlook.com>
2021-11-24 11:11:57 +0100

    fixed clock, update when scroll no

--- dashboard/main.js
+++ dashboard/main.js
@@ -221,10 +221,10 @@ ipcMain.on("closeTicket", function(e, obj){
 })
 
 ipcMain.on("refreshMessages", function(e, id) {
-    var title
-    var desc
-    var email
-    var status
+    let title
+    let desc
+    let email
+    let status
     pool.getConnection((err, connection) => {
         if(err) throw err;
         console.log('connected as id ' + connection.threadId);
@@ -234,51 +234,6 @@ ipcMain.on("refreshMessages", function(e, id) {
             desc = rows[0].message
             email = rows[0].email
             status = rows[0].status
-            console.log(rows)
-        })
-        connection.query('SELECT * FROM log WHERE ticket_id = ?',[id], (err, rows) => {
-            if(err) throw err;
-            connection.release();
-            if (rows.length>0) {
-                rows.forEach(element => {
-                    e.sender.send("logLoadedReply", {
-                        id: element.ticket_id,
-                        from: element.message_from,
-                        message: element.message_text,
-                        date: element.date,
-                        elements: rows.length,
-                        title: title,
-                        description: desc,
-                        email: email,
-                        status: status
-                    })
-                });
-            } else {
-                e.sender.send("logLoadedReply", {
-                    id: 0,
-                    title: title,
-                    description: desc
-                }) 
-            }
-        })
-    });
-});
-
-ipcMain.on("refreshMessages", function(e, id) {
-    var title
-    var desc
-    var email
-    var status
-    pool.getConnection((err, connection) => {
-        if(err) throw err;
-        console.log('connected as id ' + connection.threadId);
-        connection.query('SELECT * FROM messages WHERE id = ?',[id], (err, rows) => {
-            if(err) throw err;
-            title = rows[0].title
-            desc = rows[0].message
-            email = rows[0].email
-            status = rows[0].status
-            console.log(rows)
         })
         connection.query('SELECT * FROM log WHERE ticket_id = ?',[id], (err, rows) => {
             if(err) throw err;
@@ -309,10 +264,10 @@ ipcMain.on("refreshMessages", function(e, id) {
 });
 
 ipcMain.on("UpdateMessageDB", function (e, id) {
-    var title
-    var desc
-    var email
-    var status
+    let title
+    let desc
+    let email
+    let status
     pool.getConnection((err, connection) => {
         if(err) throw err;
         console.log('connected as id ' + connection.threadId);
@@ -350,6 +305,29 @@ ipcMain.on("UpdateMessageDB", function (e, id) {
             }
         })
     });  
+})
+
+ipcMain.on("updateMessages", function (e) {
+    pool.getConnection((err, connection) => {
+        if(err) throw err;
+        console.log('connected as id ' + connection.threadId);
+        connection.query('SELECT * FROM messages WHERE status = "open"', (err, rows) => {
+            if(err) throw err;
+            connection.release();
+            rows.forEach(element => {
+                e.sender.send("ticketsLoadedReply2", {
+                    id: element.id, 
+                    from: element.email,
+                    title: element.title,
+                    message: element.message, 
+                    status: element.status,
+                    phonenum: element.phonenum,
+                    date: element.date,
+                    name: element.name
+                })
+            });
+        })
+    });
 })
 
 app.whenReady().then(() => {
